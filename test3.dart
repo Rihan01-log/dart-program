3a74177d7a25c75c563b27ee4b0e5c51fa1c304d
Rihan01-log <rihanrasheed201@gmail.com>
2024-11-13 14:49:57 +0530

    scs

--- test3.dart
+++ test3.dart
@@ -15,19 +15,53 @@
 // }
 
 
-void main(){
-  // missingNumber([2, 0, 1, 4]) ➞ 3
-// missingNumber([0, 1]) ➞ 2
-// missingNumber([4, 2, 3, 5, 0]) ➞ 1
+// void main(){
+//   // missingNumber([2, 0, 1, 4]) ➞ 3
+// // missingNumber([0, 1]) ➞ 2
+// // missingNumber([4, 2, 3, 5, 0]) ➞ 1
 
+// // }
+
+// List a=[4, 2, 3, 5, 0];
+
+// int n=a.length;
+// int miss=n*(n+1)~/2;
+// int sum=a.reduce((a,b)=>a+b);
+// print(miss-sum);
+  
 // }
 
-List a=[2,0,1,4];
+class Solution {
+  int balancedStringSplit(String s) {
+    int balance = 0;
+    int b = 0;
 
-int n=a.length;
-int miss=n*(n+1)~/2;
-int sum=a.reduce((a,b)=>a+b);
-print(miss-sum);
-  
+    for (int i = 0; i < s.length; i++) {
+      if (s[i] == 'R') {
+        balance++;
+      } else if (s[i] == 'L') {
+        balance--;
+      }
+      if (balance == 0) {
+        b++;
+      }
+    }
+    return b;
+  }
 }
+
+void main() {
+  Solution solution = Solution();
+
+  // Example test cases
+  String s1 = "RLRRLLRLRL";
+  print(solution.balancedStringSplit(s1)); // Output: 4
+
+  String s2 = "RLLLLRRRLR";
+  print(solution.balancedStringSplit(s2)); // Output: 3
+
+  String s3 = "LLLLRRRR";
+  print(solution.balancedStringSplit(s3)); // Output: 1
+}
+
 
