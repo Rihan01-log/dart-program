6f1dc80152519befe9bf3bee8af77f4da5b60be1
Rihan01-log <rihanrasheed201@gmail.com>
2024-11-14 15:54:30 +0530

    adcdc

--- test3.dart
+++ test3.dart
@@ -31,37 +31,71 @@
   
 // }
 
-class Solution {
-  int balancedStringSplit(String s) {
-    int balance = 0;
-    int b = 0;
+// class Solution {
+//   int balancedStringSplit(String s) {
+//     int balance = 0;
+//     int b = 0;
 
-    for (int i = 0; i < s.length; i++) {
-      if (s[i] == 'R') {
-        balance++;
-      } else if (s[i] == 'L') {
-        balance--;
-      }
-      if (balance == 0) {
-        b++;
+//     for (int i = 0; i < s.length; i++) {
+//       if (s[i] == 'R') {
+//         balance++;
+//       } else if (s[i] == 'L') {
+//         balance--;
+//       }
+//       if (balance == 0) {
+//         b++;
+//       }
+//     }
+//     return b;
+//   }
+// }
+
+// void main() {
+//   Solution solution = Solution();
+
+//   // Example test cases
+//   String s1 = "RLRRLLRLRL";
+//   print(solution.balancedStringSplit(s1)); // Output: 4
+
+//   String s2 = "RLLLLRRRLR";
+//   print(solution.balancedStringSplit(s2)); // Output: 3
+
+//   String s3 = "LLLLRRRR";
+//   print(solution.balancedStringSplit(s3)); // Output: 1
+// }
+
+class Solution {
+  int countSegments(String s) {
+    if (s.trim().isEmpty) {
+      return 0;
+    }
+    List<String> words = s.trim().split(' ');
+    int count = 0;
+
+    for (String word in words) {
+      if (word.isNotEmpty) {
+        count++;
       }
     }
-    return b;
+
+    return count;
   }
+  
+
 }
 
 void main() {
   Solution solution = Solution();
-
-  // Example test cases
-  String s1 = "RLRRLLRLRL";
-  print(solution.balancedStringSplit(s1)); // Output: 4
-
-  String s2 = "RLLLLRRRLR";
-  print(solution.balancedStringSplit(s2)); // Output: 3
-
-  String s3 = "LLLLRRRR";
-  print(solution.balancedStringSplit(s3)); // Output: 1
+  String test1 = "Hello, my name is ChatGPT";
+  String test2 = "  This  is   a test   ";
+  String test3 = "   ";
+  String test4 = "Dart is fun!";
+  String test5 = "";
+  print('Segments in "$test1": ${solution.countSegments(test1)}'); // Output: 5
+  print('Segments in "$test2": ${solution.countSegments(test2)}'); // Output: 4
+  print('Segments in "$test3": ${solution.countSegments(test3)}'); // Output: 0
+  print('Segments in "$test4": ${solution.countSegments(test4)}'); // Output: 3
+  print('Segments in "$test5": ${solution.countSegments(test5)}'); // Output: 0
 }
 
 
